87cf6832e215acc033817b358052c574df9c1e7c
loganmsurber-ai <loganmsurber@gmail.com>
2025-09-26 10:19:53 -0400

    fix

--- flighttracker/src/main/java/cs220/flighttracker/OpenSkyClient.java
+++ flighttracker/src/main/java/cs220/flighttracker/OpenSkyClient.java
@@ -6,23 +6,5 @@
         public static void main(String[] args) {
             // Replace with your OpenSky username and password if needed
             OpenSkyApi api = new OpenSkyApi("loganmsurber@gmail.com", "5mg4#3_x.gLG@Nm"); 
-
-            try {
-                // Retrieve current state vectors
-                OpenSkyStates os = api.getStates(0, null, null, null); 
-
-                if (os != null && os.getStates() != null) {
-                    for (StateVector state : os.getStates()) {
-                        System.out.println("ICAO24: " + state.getIcao24() + 
-                                           ", Latitude: " + state.getLatitude() + 
-                                           ", Longitude: " + state.getLongitude());
-                    }
-                } else {
-                    System.out.println("No state vectors found.");
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
