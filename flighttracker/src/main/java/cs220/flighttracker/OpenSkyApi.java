347b2279cbfcdd452388d68fa4f44afdbaf3b2ad
loganmsurber-ai <loganmsurber@gmail.com>
2025-09-26 10:35:43 -0400

    change name

---
+++ flighttracker/src/main/java/cs220/flighttracker/OpenSkyApi.java
@@ -0,0 +1,11 @@
+import org.opensky.api.OpenSkyApi;
+import org.opensky.api.OpenSkyStates;
+import org.opensky.model.StateVector;
+
+    public class OpenSkyClient {
+        public static void main(String[] args) {
+            // Replace with your OpenSky username and password if needed
+            OpenSkyApi api = new OpenSkyApi("loganmsurber@gmail.com", "5mg4#3_x.gLG@Nm");
+            
+        }
+    }
